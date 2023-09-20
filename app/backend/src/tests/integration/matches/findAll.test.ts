e6e952e2a8e928419fe5ecefac8cab784b0c779f
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-09-20 18:37:56 -0300

    :test_tube: test: Adicionado test de integração para findAll da rota /matches.

---
+++ app/backend/src/tests/integration/matches/findAll.test.ts
@@ -0,0 +1,36 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import { match, matches  } from '../../mocks/match.mock';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../../../app';
+import SequelizeMatchModel from '../../../database/models/SequelizeMatchModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Integração - FindAll', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(SequelizeMatchModel, "findAll")
+      .resolves( matches as SequelizeMatchModel[] );
+    
+  });
+
+  after(()=>{
+    (SequelizeMatchModel.findAll as sinon.SinonStub).restore();
+  })
+
+  it('Verifica se uma requisição para a rota /matches retorna um objeto com status 200 e body do tipo SequelizeMatchModel.', async function() {
+    const { status, body } = await chai.request(app).get('/matches');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matches);
+  });
+ 
+});
---
+++ app/backend/src/tests/mocks/match.mock.ts
@@ -0,0 +1,15 @@
+const match = {
+    id: 1,
+    homeTeamId: 1,
+    homeTeamGoals: 1,
+    awayTeamId: 2,
+    awayTeamGoals: 3,
+    inProgress: false,
+};
+
+const matches = [match];
+  
+  export {
+    match,
+    matches,
+  };
