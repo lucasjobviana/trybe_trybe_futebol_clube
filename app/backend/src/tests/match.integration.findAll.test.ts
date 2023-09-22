1f7faf9bd7eddd569b18f64dda9d341d4034ea38
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-09-22 04:37:34 -0300

    :test_tube: test: Adicionado teste para as novas rotas.

--- app/backend/src/tests/match.integration.findAll.test.ts
+++ app/backend/src/tests/match.integration.findAll.test.ts
@@ -4,8 +4,8 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { matchesWithTeams, matchesWithTeamsSequelize  } from './mocks/match.mock';
 import SequelizeMatchModel from '../database/models/SequelizeMatchModel';
+import { matchesBothTEAM, matchesBothTEAM_Sequelize, matchesFalseTEAM, matchesFalseTEAM_Sequelize, matchesTrueTEAM, matchesTrueTEAM_Sequelize } from './mocks/match.mock';
 
 
 chai.use(chaiHttp);
@@ -13,23 +13,38 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Integração - Matches - GET', () => {
-  let chaiHttpResponse: Response;
 
-  before(async () => {
+  it('Retorna um objeto com status 200 e um objeto do tipo Match[].', async function() {
     sinon
-      .stub(SequelizeMatchModel, "findAll")
-      .resolves( matchesWithTeamsSequelize as any);
-    
-  });
-
-  after(()=>{
-    (SequelizeMatchModel.findAll as sinon.SinonStub).restore();
-  })
-
-  it('Retorna um objeto com status 200 e um objeto do tipo SequelizeMatchModel.', async function() {
+    .stub(SequelizeMatchModel, "findAll")
+    .resolves( matchesBothTEAM_Sequelize as any);
     const { status, body } = await chai.request(app).get('/matches');
     expect(status).to.equal(200);
-    expect(body).to.deep.equal(matchesWithTeams);
+    expect(body).to.deep.equal(matchesBothTEAM);
+    (SequelizeMatchModel.findAll as sinon.SinonStub).restore();
+  });
+
+  it('Retorna um objeto com status 200 e um objeto do tipo Match[] com inProgress=true.', async function() {
+    sinon
+    .stub(SequelizeMatchModel, "findAll")
+    .resolves( matchesTrueTEAM_Sequelize as any);
+    const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesTrueTEAM);
+    (SequelizeMatchModel.findAll as sinon.SinonStub).restore();
+  });
+
+  it('Retorna um objeto com status 200 e um objeto do tipo Match[] com inProgress=false.', async function() {
+    sinon
+    .stub(SequelizeMatchModel, "findAll")
+    .resolves( matchesFalseTEAM_Sequelize as any);
+    const { status, body } = await chai.request(app).get('/matches?inProgress=false');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesFalseTEAM);
+    (SequelizeMatchModel.findAll as sinon.SinonStub).restore();
   });
  
 });
+
+
--- app/backend/src/tests/mocks/match.mock.ts
+++ app/backend/src/tests/mocks/match.mock.ts
@@ -3,17 +3,26 @@ import { IMatch } from "../../interfaces/IMatch";
 import { TSequelizeMatchWithTeams } from "../../interfaces/types/TSequelizeMatchWithTeams";
 import SequelizeTeamModel from "../../database/models/SequelizeTeamModel";
 
-const match:IMatch = {
+const matchFalse:IMatch = {
     id: 1,
     homeTeamId: 1,
     homeTeamGoals: 1,
     awayTeamId: 2,
-    awayTeamGoals: 3,
+    awayTeamGoals: 2,
     inProgress: false,
 };
 
-const matchWithTeamsSequelize:TSequelizeMatchWithTeams = {
-  ...match,
+const matchTrue:IMatch = {
+  id: 2,
+  homeTeamId: 2,
+  homeTeamGoals: 2,
+  awayTeamId: 1,
+  awayTeamGoals: 1,
+  inProgress: true,
+};
+
+const matchFalseTEAM_Sequelize:TSequelizeMatchWithTeams = {
+  ...matchFalse,
   homeTeam: {dataValues:{
     id: 1,
     teamName: 'Team 1',
@@ -24,21 +33,46 @@ const matchWithTeamsSequelize:TSequelizeMatchWithTeams = {
   }} as SequelizeTeamModel,
 };
 
-const matchWithTeams = {
-  ...match,
+const matchTrueTEAM_Sequelize:TSequelizeMatchWithTeams = {
+  ...matchTrue,
+  homeTeam: {dataValues:{
+    id: 2,
+    teamName: 'Team 2',
+  }} as SequelizeTeamModel,
+  awayTeam: {dataValues:{
+    id: 1,
+    teamName: 'Team 1',
+  }} as SequelizeTeamModel,
+};
+
+const matchFalseTEAM = {
+  ...matchFalse,
   homeTeam: {teamName: 'Team 1'},
   awayTeam: {teamName: 'Team 2'},
 }
 
-const matches = [match];
-const matchesWithTeams = [matchWithTeams];
-const matchesWithTeamsSequelize = [matchWithTeamsSequelize];
-  
+const matchTrueTEAM = {
+  ...matchTrue,
+  homeTeam: {teamName: 'Team 2'},
+  awayTeam: {teamName: 'Team 1'},
+}
+ 
+
+const matches = [matchFalse];
+const matchesBothTEAM = [matchFalseTEAM,matchTrueTEAM];
+const matchesTrueTEAM = [matchTrueTEAM];
+const matchesFalseTEAM = [matchFalseTEAM];
+const matchesBothTEAM_Sequelize = [matchFalseTEAM_Sequelize,matchTrueTEAM_Sequelize];
+const matchesTrueTEAM_Sequelize = [matchTrueTEAM_Sequelize];
+const matchesFalseTEAM_Sequelize = [matchFalseTEAM_Sequelize];
+//Both, True, False: InProgress
+//TEAM: Includes homeTeam and awayTeam objects
+//_Sequelize: Objects returned by Sequelize   
   export {
-    match,
-    matchWithTeams,
-    matchWithTeamsSequelize, 
-    matches,
-    matchesWithTeams,
-    matchesWithTeamsSequelize,
+    matchesBothTEAM,
+    matchesBothTEAM_Sequelize,
+    matchesTrueTEAM,
+    matchesTrueTEAM_Sequelize,
+    matchesFalseTEAM,
+    matchesFalseTEAM_Sequelize,
   };
